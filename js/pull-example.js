2cac9e09bcf6d3ab7e4d98af253d201776b009cd
Arthur Verger <arthur.verger@gmail.com>
2013-07-09 16:35:11 +0100

    ok

---
+++ js/pull-example.js
@@ -0,0 +1,66 @@
+/*jslint browser: true, sloppy: true, white: true, nomen: true, plusplus:true, maxerr: 50, indent: 2 */
+/*global jQuery:false, iScroll:false */
+
+/*jshint forin:true, noarg:true, noempty:true, eqeqeq:true, bitwise:true, strict:true,
+ undef:true, curly:true, browser:true, jquery:true, indent:2, maxerr:50,
+ white:false, nomen:false */
+
+//-------------------------------------------------------
+// Pull-Up and Pull-Down callbacks for "Pull" page
+//-------------------------------------------------------
+(function pullPagePullImplementation($) {
+    "use strict";
+    var pullDownGeneratedCount = 0,
+        listSelector = "div.pull-demo-page ul.ui-listview";
+
+
+    // This is the callback that is called when the user has completed the pull-down gesture.
+    // Your code should initiate retrieving data from a server, local database, etc.
+    // Typically, you will call some function like jQuery.ajax() that will make a callback
+    // once data has been retrieved.
+    //
+    // For demo, we just use timeout to simulate the time required to complete the operation.
+    function onPullDown (event, data) {
+
+
+        $.support.cors = true;
+        promise.ajaxTimeout = 10000;
+        promise.get('http://tuple-center.herokuapp.com/take/user1/notifications.json?passwd=356a192b7913b04c54574d18c28d46e6395428ab&random='+Math.random()).then(function(error, text, xhr) {
+
+
+
+
+            //     if (error) {
+//                $("#debug").html('ERROR');
+//                document.getElementById("loader").style.display = 'none';
+//                document.getElementById("issueNetwork").style.display = 'inline';
+//                return;
+//            }
+
+            var jsonArray = JSON.parse(text);
+            var newContent = "";
+
+            for (var i=1; i < jsonArray.length; i++){
+                var values = jsonArray[i].value.split('##');
+                newContent = '<a href="#elem?val='+values + '" data-transition="slide"><img src="img/eat2.jpg" class="shadow" /></a>' + newContent;
+            }
+            $(listSelector).prepend(newContent).listview("refresh");  // Prepend new content and refresh listview
+            data.iscrollview.refresh();    // Refresh the iscrollview
+        });
+
+
+
+
+    }
+
+
+
+    // Set-up jQuery event callbacks
+    $(document).delegate("div.pull-demo-page", "pageinit",
+        function bindPullPagePullCallbacks(event) {
+            $(".iscroll-wrapper", this).bind( {
+                iscroll_onpulldown : onPullDown
+            } );
+        } );
+
+}(jQuery));
